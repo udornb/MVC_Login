238d33b521c895a46e12110d454cf80be2c78367
udornb <udornrt@gmail.com>
2022-04-07 22:40:27 +0700

    on work:

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//refactoring @ 26/03/2021
+$(function () {
+    //innitail login 
+    //remove current 
+    try {
+        localStorage.setItem('x', getTodayDate(0));        
+    } catch
+    {  
+         alert('Error in cache!'); 
+    }
+
+   
+    var recno = sessionStorage.getItem("Login_Success");
+    if ((recno === '') || (recno === null) || (recno === undefined)) { 
+        $('#modal-login').modal({
+            backdrop: 'static',
+            keyboard: false
+         });       
+        $('#modal-login').modal('show');
+    } else {      
+
+    }
+});
+
+$('#modal-login').on('hidden.bs.modal', function (e) {
+    var recno = sessionStorage.getItem("Login_Success");
+    if ((recno === '') || (recno === null) || (recno === 'undefined')) {
+        //alert('Login page require!');
+        $('#modal-login').modal('show');
+    } else {
+        //setTimeout(function () { dialog_get_staffconsent(); }, 2000);
+        //console.log(recno);
+    }
+});
